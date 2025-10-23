137a6a887ee43dcd5efb91f32b140b9607b9bbc4
steveparker123 <176498134+steveparker123@users.noreply.github.com>
2025-10-23 03:58:05 -0700

    update: type script error

--- src/pages/app/Home.tsx
+++ src/pages/app/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { dotBar } from "../../assets/export.ts";
 import { HomeFilterDropDown } from "../../components/global/FilterDropDown.tsx";
